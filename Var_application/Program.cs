db4237bf559f975e4905628213a1481cb84f9875
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2024-02-29 11:14:57 +0200

    Add files via upload

---
+++ Var_application/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Var_application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 19; //explicitly typed variable
+            Console.WriteLine($"My number is: {myNumber}");
+
+            string myName = "Joe";//explicitly typed variable
+            Console.WriteLine($"My Name is: {myName}");
+
+            var newNumber = 15; //implicitly typed variable
+            Console.WriteLine($"The new number is: {newNumber}");
+
+            var newName = "Jane";//implicitly typed variable
+            Console.WriteLine($"Your name is: {newName}");
+
+            int myInt = newNumber;
+
+        }
+    }
+}
