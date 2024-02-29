9374fbe35935c925b34d95cb8a6cdf5db438c5b8
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2024-02-29 11:07:09 +0200

    Add files via upload

---
+++ HelloWorld_App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables to hold info
+            string name = "Joe Blog";
+            string workPhoneNumber = "031-209-4567";
+            string cellPhoneNumber = "082-123-4567";
+            DateTime birthDate = new DateTime(1990, 5, 24);//Year, Month,Day
+            //DateTime ia a value defined type found in the .Net Framework to represent dates & time
+
+            //Display the above person's details on console
+            Console.WriteLine("Personal Information");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Work Number: {workPhoneNumber}");
+            Console.WriteLine($"Cellphone Number: {cellPhoneNumber}");
+            Console.WriteLine($"Birth Date: {birthDate}");
+        }
+    }
+}
